571a59c81411a266fc0f692b5c34c64ae2374260
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-08-26 13:35:27 +0900

    Update kubernetes IP

--- src/libs/api/client.js
+++ src/libs/api/client.js
@@ -1,7 +1,8 @@
 import axios from '../../../node_modules/axios/index';
 
 const client = axios.create({
-  baseURL: 'http://192.168.56.33:8080',
+  baseURL: 'http://192.168.56.31:30003',
+  // baseURL: 'http://192.168.56.33:8080',
   // baseURL: 'http://localhost:8080',
   //   baseURL: 'http://13.124.143.225:8080',
 });
