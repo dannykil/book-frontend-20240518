b6f51f6c4a8c97f7b93caab0f34646b178c71d66
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-09-01 13:27:33 +0900

    webhook 11

--- src/components/category/CategoryHeader.js
+++ src/components/category/CategoryHeader.js
@@ -84,7 +84,7 @@ const CategoryHeader = ({
   return (
     <>
       <Card className="mb-2">
-        <Card.Header as="h3">Category {title} 10</Card.Header>
+        <Card.Header as="h3">Category {title} 11</Card.Header>
         <Card.Body>
           <Card.Text>
             With supporting text below as a natural lead-in to additional
