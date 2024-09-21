ee1fd5a7afe4fd6c4cbca45f24dcfa28c7836468
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-09-21 18:27:25 +0900

    changing backend server

--- src/libs/api/client.js
+++ src/libs/api/client.js
@@ -1,11 +1,12 @@
 import axios from '../../../node_modules/axios/index';
 
 const client = axios.create({
+  // baseURL: 'http://13.124.143.225:8080',
   // baseURL: 'http://192.168.56.31:30003',
-  baseURL: 'http://192.168.56.32:30003',
+  // baseURL: 'http://192.168.56.32:30003',
   // baseURL: 'http://192.168.56.33:8080',
   // baseURL: 'http://localhost:8080',
-  //   baseURL: 'http://13.124.143.225:8080',
+  baseURL: 'book-backend-svc-1.book-dev.svc.cluster.local:8080',
 });
 
 export default client;
