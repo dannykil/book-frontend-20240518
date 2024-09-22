0146c7e240bb0e38349064c523778264245e4795
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-09-22 14:17:02 +0900

    adding http

--- src/libs/api/client.js
+++ src/libs/api/client.js
@@ -6,7 +6,7 @@ const client = axios.create({
   // baseURL: 'http://192.168.56.32:30003',
   // baseURL: 'http://192.168.56.33:8080',
   // baseURL: 'http://localhost:8080',
-  baseURL: 'book-backend-svc-1.book-dev.svc.cluster.local:8080',
+  baseURL: 'http://book-backend-svc-1.book-dev.svc.cluster.local:8080',
 });
 
 export default client;
